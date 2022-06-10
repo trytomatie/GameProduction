635f272fb09b11e272e8892da0b76532b8c29b5c
trytomatie <trytomatie@gmail.com>
2022-06-10 23:38:56 +0200

    Rolltreppen und level

---
+++ New Unity Project/Assets/Escalator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escalator : MonoBehaviour
+{
+    public GameObject stairPice;
+    public int pieces = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < pieces;i++)
+        {
+            GameObject go = Instantiate(stairPice, transform.position, transform.rotation, transform);
+            go.GetComponent<Animator>().SetFloat("Offset", (float)i / (float)pieces);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
