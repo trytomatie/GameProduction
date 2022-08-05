61c6a49b2990a53f03c05d64d659576d58bf37ab
trytomatie <trytomatie@gmail.com>
2022-08-05 02:20:12 +0200

    Implemented Elevator, created Win screen (Not finished)

---
+++ GameProductionGame/Assets/Elevator/FakeFloor.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FakeFloor : MonoBehaviour
+{
+    public List<Transform> fakeFloors;
+    public TextMeshPro floorText;
+    public float fakeFloorOffset;
+
+    private Dictionary<Transform, Vector3> basePositions = new Dictionary<Transform, Vector3>();
+    // Update is called once per frame
+
+    private void Start()
+    {
+        foreach (Transform fakeFloor in fakeFloors)
+        {
+            basePositions.Add(fakeFloor, fakeFloor.position);
+        }
+    }
+    void Update()
+    {
+        foreach(Transform fakeFloor in fakeFloors)
+        {
+            fakeFloor.position = basePositions[fakeFloor] + new Vector3(0, fakeFloorOffset %8, 0);
+        }
+        floorText.text = "U " + (4 + Mathf.RoundToInt(fakeFloorOffset / 8));
+    }
+}
